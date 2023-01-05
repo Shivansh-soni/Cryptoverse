e1db6a36b47a476fd19f17c80310113a7ebd220c
Shivansh-soni <shivanshsoni.indore@gmail.com>
2023-01-05 20:57:02 +0530

    locate2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-
-import { BrowserRouter as Router } from "react-router-dom";
-import Animated from "./New/animated routes/Animated.";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+// import Animated from "./New/animated routes/Animated.";
 import Navbar from "./New/Navbar/Navbar";
+import { Header, News, Currency } from "./New";
 
 const App = () => {
+  const location = useLocation();
   return (
     <div>
       {/* <div className="page-loader z-999">
@@ -27,7 +33,16 @@ const App = () => {
 
         <div className="data bg-base-100 drop-shadow-xl ">
           <Router>
-            <Animated />
+            <Routes
+              location={location}
+              key={location.pathname}
+              initial={false}
+              mode={"wait"}
+            >
+              <Route path="/currency" element={<Currency />} />
+              <Route path="/" element={<Header />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
           </Router>
         </div>
       </div>
